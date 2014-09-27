fe0c9efedc70ab7474cc83c162a7ecdb84555149
Martina Radeva <martaradeva@yahoo.com>
2014-09-28 00:21:31 +0300

    n-factorial working

--- factorial.rb
+++ factorial.rb
@@ -1,7 +1,16 @@
+class Fixnum
+  def fact_iterative
+    f = 1; for i in 1..self; f *= i; end; f
+  end
+end
+
 def kth_factorial(k, n)
   k.inspect
-  if not k.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
-    elsif not n.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
-      else puts "Great! Starting computation!"
+  if not k.is_a? Integer or not n.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
+    elsif (k == 0) or (n == 0) then puts "0"
+      else
+        result = n.fact_iterative
+      # puts "Great! Starting computation!"
   end
+  puts result
 end
