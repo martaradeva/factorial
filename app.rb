30bdff3a15233ac140a74279c13cf8d7a6740f29
Martina Radeva <martaradeva@yahoo.com>
2014-09-27 23:57:14 +0300

    first commit

---
+++ app.rb
@@ -0,0 +1,7 @@
+require './factorial'
+
+puts "Enter K"
+k = gets.chomp.to_i
+puts "Enter N"
+n = gets.chomp.to_i
+kth_factorial(k, n)
---
+++ factorial.rb
@@ -0,0 +1,7 @@
+def kth_factorial(k, n)
+  k.inspect
+  if not k.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
+    elsif not n.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
+      else puts "Great! Starting computation!"
+  end
+end
