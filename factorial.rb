dbd16708f920173f66743b6bc6a688b86a72ef14
Martina Radeva <martaradeva@yahoo.com>
2014-09-28 00:47:34 +0300

    k-th multiple working, wip output format

--- factorial.rb
+++ factorial.rb
@@ -9,7 +9,9 @@ def kth_factorial(k, n)
   if not k.is_a? Integer or not n.is_a? Integer then puts "Invalid input. Both K and N need to be Integer numbers!"
     elsif (k == 0) or (n == 0) then puts "0"
       else
-        result = n.fact_iterative
+        f = n.fact_iterative
+        result = 1
+        for i in 1..k do result = result*f end
       # puts "Great! Starting computation!"
   end
   puts result
